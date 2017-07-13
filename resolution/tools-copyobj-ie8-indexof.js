a4ae314dceeee88c1139d9d5b66fa10e6fc551e2
bearface <314778394@qq.com>
2017-07-13 20:06:32 +0800

    1

--- resolution/tools-copyobj-ie8-indexof.js
+++ resolution/tools-copyobj-ie8-indexof.js
@@ -119,3 +119,20 @@ function delSpecialChar (strs) {
     //删除特殊字符
     return strs.replace(/[<|>|{|}|(|)]/g, "");
 }
+function selectText(element) {
+    var text = document.getElementById(element);
+    if (document.body.createTextRange) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        /*if(selection.setBaseAndExtent){
+            selection.setBaseAndExtent(text, 0, text, 1);
+        }*/
+    } 
+}
